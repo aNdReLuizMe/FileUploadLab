777548cb554f1a1dae81e8de03114ac09c406e05
André Luiz <andreluiz.me@gmail.com>
2024-07-16 19:04:20 -0400

    COntinuação

--- database/migrations/2024_07_16_195339_create_patients_table.php
+++ database/migrations/2024_07_16_195339_create_patients_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->date('date_of_birth');
+
+            $table->foreignId('owner_id')
+                ->constrained('owners')
+                ->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
--- database/migrations/2024_07_16_195339_create_treatments_table.php
+++ database/migrations/2024_07_16_195339_create_treatments_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('treatments', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->text('notes')->nullable();
+
+            $table->foreign('patient_id')
+                ->constrained('patients')
+                ->cascadeOnDelete();
+
+            $table->unsignedBigInteger('price')->nullable();
+
+
             $table->timestamps();
         });
     }
